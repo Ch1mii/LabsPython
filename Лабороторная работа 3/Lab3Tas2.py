2e0cb7ed21f283121929cf1bd8b21110d8f0bdba
Ch1mii <96430671+Ch1mii@users.noreply.github.com>
2024-11-02 15:29:36 +0300

    Add files via upload

---
+++ Лабороторная работа 3/Lab3Tas2.py
@@ -0,0 +1,15 @@
+def find_common_participants(group1, group2, delimiter=','):
+
+    participants_group1 = set(group1.split(delimiter))
+    participants_group2 = set(group2.split(delimiter))
+
+    common_participants = participants_group1.intersection(participants_group2)
+
+    return sorted(common_participants)
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group, delimiter='|')
+print("Общие участники:", common_participants)
